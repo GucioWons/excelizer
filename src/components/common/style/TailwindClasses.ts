7dc4d546780bccb96315145c5a6440525824fece
GucioWons <wgutek1@wp.pl>
2025-07-26 14:50:58 +0200

    Moved-edit-cell-buton-style-to-tailwind-classes

---
+++ src/components/common/style/TailwindClasses.ts
@@ -0,0 +1,9 @@
+export const cell = {
+
+}
+
+export const editCellButton = {
+    base: 'fixed bottom-4 right-4 px-4 py-2 rounded border font-semibold transition',
+    enabled: 'bg-[#107C41] text-white hover:bg-[#0e6e39] border-[#0e6e39]',
+    disabled: 'bg-gray-200 text-gray-500 border-gray-300 cursor-not-allowed'
+}

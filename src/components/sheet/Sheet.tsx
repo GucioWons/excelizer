7fe1a869e30f9182ac91c52d1f7c213c18883908
GucioWons <wgutek1@wp.pl>
2025-07-26 14:30:51 +0200

    Disabling-edit-cell-button

--- src/components/sheet/Sheet.tsx
+++ src/components/sheet/Sheet.tsx
@@ -76,7 +76,11 @@ function Sheet() {
         </div>
     <button
         onClick={() => {}}
-        className="fixed bottom-4 right-4 bg-blue-500 text-white px-4 py-2 rounded shadow-lg hover:bg-blue-600 transition"
+        className={`fixed bottom-4 right-4 px-4 py-2 rounded border font-semibold transition
+        ${selectedCell
+            ? 'bg-[#107C41] text-white hover:bg-[#0e6e39] border-[#0e6e39]'
+            : 'bg-gray-200 text-gray-500 border-gray-300 cursor-not-allowed'}
+    `}
     >
         Edit Cell
     </button>

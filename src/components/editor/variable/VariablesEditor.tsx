2234b90f0b9ae3e39b69451a6ba0da23f6ea7209
GucioWons <wgutek1@wp.pl>
2025-08-06 20:04:38 +0200

    Add-new-variable

--- src/components/editor/variable/VariablesEditor.tsx
+++ src/components/editor/variable/VariablesEditor.tsx
@@ -1,4 +1,5 @@
 import {VariableType} from "./VariableType.ts";
+import {useCallback, useState} from "react";
 
 export interface VariableData {
     name: string;
@@ -7,22 +8,28 @@ export interface VariableData {
 }
 
 function VariablesEditor() {
-    const variables: VariableData[] = [
+    const initialVariables: VariableData[] = [
         {name: "name", type: VariableType.STRING, nullable: false},
         {name: "number", type: VariableType.NUMERIC, nullable: true},
         {name: "deprecated", type: VariableType.BOOLEAN, nullable: false}
     ];
 
+    const [variables, setVariables] = useState(initialVariables)
+
+    const addVariable = useCallback(() => {
+        setVariables(
+            [...variables, {name: "", type: VariableType.STRING, nullable: false}],
+        );
+    }, [variables]);
+
     return (
         <div>
             {variables.map(variable => (
                 <div>
-                    <div>{variable.name}</div>
-                    <div>{variable.type}</div>
-                    <div>{variable.nullable}</div>
+                    <div>{variable.name}, {variable.type}, {variable.nullable.toString()}</div>
                 </div>
             ))}
-            <button>Add variable</button>
+            <button onClick={addVariable}>Add variable</button>
         </div>
     );
 }
